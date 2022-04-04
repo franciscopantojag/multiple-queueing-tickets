df0dbb6ae4b6c6755fdeb220f54a0ba1c13607e6
Francisco Pantoja <franciscopantojag98@gmail.com>
2022-04-03 22:13:24 -0500

    feat: defaultUseQueueURL to false

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const defaultNumberOfQueues = 100;
 export const defaultEventURL = 'https://teleticket.com.pe/daddy-yankee-enlima2022';
 export const defaultBuyButtonCSS = "[id='choose-tickets-section text-center'] > a";
 export const defaultQueueURL = 'https://queue.teleticket.com.pe/?c=puntoticket&e=ics014b&cid=es-CL&t_cal=1&t_ct=3';
-export const defaultUseQueueURL = true;
+export const defaultUseQueueURL = false;
 
 export const defaultMultipleQueueingOptions = {
   numberOfQueues: defaultNumberOfQueues,
