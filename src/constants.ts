a8b7e1d3142b7e8e1c842a3bbeaefa3abff4df5c
Francisco Pantoja <franciscopantojag98@gmail.com>
2022-11-14 20:51:48 -0500

    chore: update event url

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const NUMBER_OF_QUEUES = 50;
-export const EVENT_URL = 'https://teleticket.com.pe/coldplay2022ln';
-export const BUY_BUTTON_CSS = "[id='choose-tickets-section'] > a";
+export const EVENT_URL = 'https://teleticket.com.pe/romeo-santos-formula-vol3-la-gira';
+export const BUY_BUTTON_CSS = "[id='choose-tickets-section text-center'] > a";
 export const QUEUE_URL = null;
 export const USE_QUEUE_URL = false;
 
