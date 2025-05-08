46950149417c72e2a8cec8646ea85ee6c5850049
Francisco Pantoja <franciscopantojag98@gmail.com>
2025-05-08 13:43:11 -0500

    segunda fecha

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ export const EVENT_URL = 'https://www.ticketmaster.pe/event/bad-bunny-debi-tirar
 export const BUY_BUTTON_XPATH =
   '//*[@id="component-4319d70d-5e30-42c1-bcb2-7ff602f44d76"]/div/div/div/div/div/div/div/div/div[3]/a';
 export const QUEUE_URL =
-  'https://www.ticketmaster.pe/event/bad-bunny-debi-tirar-mas-fotos-world-tour-2026-venta-bbva-563902';
+  'https://www.ticketmaster.pe/event/bad-bunny-debi-tirar-mas-fotos-world-tour-2026-canal-venta-2-7589289';
 export const USE_QUEUE_URL = true;
 
 export const QUEUEING_OPTIONS = {
